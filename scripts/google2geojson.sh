40e76ed5e2b8a5078a6c2c768c7195a4cf055082
John Muccigrosso <john@muccigrosso.org>
2016-07-07 18:46:08 -0400

    Improve script w/new output file

--- scripts/google2geojson.sh
+++ scripts/google2geojson.sh
@@ -2,19 +2,30 @@
 
 # Converts a Google spreadsheet to geojson
 
-destFolder='/Users/john_muccigrosso/Documents/github/local/temples/'
+# Set the target directory
+# Abstract out my local info (which also anonymizes it)
+me=`whoami`
+# Set the folder to one in the relevant github repository
+destFolder="/Users/""$me""/Documents/github/local/temples/"
+# Make sure that folder exists
+if [ ! -d $destFolder ]
+then
+    echo -e "\nTarget folder:\n\n\t"$destFolder"\n\ndoes not exist. It must. Create it before continuing.\n"
+    exit
+fi
 
 # Get the spreadsheet feed in json format
 g_json=`curl -s -stdout 'https://spreadsheets.google.com/feeds/list/1by5Xo90wcyYJBWkZs-dtqpkCnoKPlf6CJdEmyXJ4rZ4/1/public/values?alt=json'`
-# Clean it up a bit
+# Clean out some of the Google cruft and grab only the rows/entries themselves
 g_json=`echo $g_json | sed 's/gsx\\$//g' | jq -c '.feed.entry[]'`
-# Only entries that have geographical data, substituting for anything that isn't a digit or .
+# Include only entries with geographical data, after substituting for anything that isn't a digit or .
 # And yes, four backslashes are needed to get the escaping to work.
 g_json=`echo $g_json | jq 'select( (.longitude."$t" | gsub("[\\\\D\\\\.]";"")) != "")'`
 # Convert remaining entries to geojson variables
+# Currently a minimal set
 g_json=`echo $g_json | jq -c '{type: "Feature", geometry: {type: "Point", coordinates: [(.longitude."$t"|tonumber), (.latitude."$t"|tonumber)]}, "properties": {name: .temple."$t"}}' | tr '\n' ',' | sed 's/,$//g'`
 
-# Put it all together as valid geojson
+# Put it all together as valid geojson in the variable "output"
 # Start with the header
 output="{\"type\": \"FeatureCollection\",\"features\": ["  
 # Add the data from the spreadsheet
@@ -22,5 +33,5 @@ output="$output $g_json"
 # Close it up with closing brackets
 output="$output  ]}"
 
-# Create output file with prettfied data
-echo $output | jq '.' > "$destFolder""json_test.json"
+# Create output file with prettified data
+echo $output | jq '.' > "$destFolder""temples.json"
