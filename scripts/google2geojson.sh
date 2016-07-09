17d5473165be28696822b2995d3d44086e43353a
John Muccigrosso <john@muccigrosso.org>
2016-07-09 16:59:58 -0400

    Removing markdown (stupid)

--- scripts/google2geojson.sh
+++ scripts/google2geojson.sh
@@ -15,15 +15,15 @@ then
 fi
 
 # Get the spreadsheet feed in json format
-g_json=`curl -s -stdout 'https://spreadsheets.google.com/feeds/list/1by5Xo90wcyYJBWkZs-dtqpkCnoKPlf6CJdEmyXJ4rZ4/1/public/values?alt=json'`
+g_json=$(curl -s -stdout 'https://spreadsheets.google.com/feeds/list/1by5Xo90wcyYJBWkZs-dtqpkCnoKPlf6CJdEmyXJ4rZ4/1/public/values?alt=json')
 # Clean out some of the Google cruft and grab only the rows/entries themselves
-g_json=`echo $g_json | sed 's/gsx\\$//g' | jq -c '.feed.entry[]'`
+g_json=$(echo $g_json | sed 's/gsx\$//g' | jq -c '.feed.entry[]')
 # Include only entries with geographical data, after substituting for anything that isn't a digit or .
 # And yes, four backslashes are needed to get the escaping to work.
-g_json=`echo $g_json | jq 'select( (.longitude."$t" | gsub("[\\\\D\\\\.]";"")) != "")'`
+g_json=$(echo $g_json | jq 'select( (.longitude."$t" | gsub("[\\\\D\\\\.]";"")) != "")')
 # Convert remaining entries to geojson variables
 # Currently a minimal set
-g_json=`echo $g_json | jq -c '{type: "Feature", geometry: {type: "Point", coordinates: [(.longitude."$t"|tonumber), (.latitude."$t"|tonumber)]}, "properties": {name: .temple."$t"}}' | tr '\n' ',' | sed 's/,$//g'`
+g_json=$(echo $g_json | jq -c '{type: "Feature", geometry: {type: "Point", coordinates: [(.longitude."$t"|tonumber), (.latitude."$t"|tonumber)]}, "properties": {name: .name."$t"}}' | tr '\n' ',' | sed 's/,$//g')
 
 # Put it all together as valid geojson in the variable "output"
 # Start with the header
@@ -31,7 +31,7 @@ output="{\"type\": \"FeatureCollection\",\"features\": ["
 # Add the data from the spreadsheet
 output="$output $g_json"
 # Close it up with closing brackets
-output="$output  ]}"
+output="$output ]}"
 
 # Create output file with prettified data
-echo $output | jq '.' > "$destFolder""temples.json"
+echo $output | jq '.' > "/Users/john_muccigrosso/Downloads/temples.json"
