06fd19d9c58fcbce826ff6fb0f862436f05b202f
John Muccigrosso <john@muccigrosso.org>
2016-07-09 19:54:23 -0400

    Clean json more.
    Cleaning the Google json further before processing it into geojson.

--- scripts/google2geojson.sh
+++ scripts/google2geojson.sh
@@ -16,13 +16,14 @@ fi
 
 # Get the spreadsheet feed in json format
 g_json=$(curl -s -stdout 'https://spreadsheets.google.com/feeds/list/1by5Xo90wcyYJBWkZs-dtqpkCnoKPlf6CJdEmyXJ4rZ4/1/public/values?alt=json')
-# Clean out some of the Google cruft and grab only the rows/entries themselves
-g_json=$(echo $g_json | sed 's/gsx\$//g' | jq -c '.feed.entry[]')
+# Grab only the rows/entries & clean out some Google cruft: gsx$ prefix; unnecessary $t attribute using perl since sed doesn't support the non-greedy operater ?
+g_json=$(echo $g_json | jq -c '.feed.entry[]' | sed 's/gsx\$//g' | perl -pe 's/:{"\$t":(".*?")}/:\1/g')
 # Include only entries with geographical data, after substituting for anything that isn't a digit or .
-g_json=$(echo $g_json | jq 'select( (.longitude."$t" | gsub("[\\D\\.]";"")) != "")')
+g_json=$(echo $g_json | jq 'select( (.longitude | gsub("[\\D\\.]";"")) != "")')
+
 # Convert remaining entries to geojson variables
 # Currently a minimal set
-g_json=$(echo $g_json | jq -c '{type: "Feature", geometry: {type: "Point", coordinates: [(.longitude."$t"|tonumber), (.latitude."$t"|tonumber)]}, "properties": {name: .name."$t"}}' | tr '\n' ',' | sed 's/,$//g')
+g_json=$(echo $g_json | jq -c '{type: "Feature", geometry: {type: "Point", coordinates: [(.longitude|tonumber), (.latitude|tonumber)]}, "properties": {name: .name}}' | tr '\n' ',' | sed 's/,$//g')
 
 # Put it all together as valid geojson in the variable "output"
 # Start with the header
