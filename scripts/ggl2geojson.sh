933a799d7152f809b27187c5caa8f26dd283e280
John Muccigrosso <john@muccigrosso.org>
2016-10-16 20:11:29 -0400

    New command location & file check.

--- scripts/ggl2geojson.sh
+++ scripts/ggl2geojson.sh
@@ -9,8 +9,9 @@ dest="/Users/$me/Documents/github/local/temples"
 temp=$(echo $TMPDIR | sed 's:/$::')
 
 # Path to include homebrew stuff like jq for running via launchctl
-binaryPath=$(dirname $(which jq))
-export PATH=$PATH:$binaryPath
+jqPath=$(dirname $(which jq))
+ogrPath=$(dirname $(which ogr2ogr))
+export PATH=$jqPath:$ogrPath:$PATH
 
 # Create the vrt file for the conversion from csv to geojson
 text="      <OGRVRTDataSource>"
@@ -44,26 +45,33 @@ rm "$temp/sheet.json" 2>/dev/null
 ogr2ogr -skipfailures -f geojson "$temp/sheet.json" "$temp/sheet.vrt"
 
 # Clear false 0,0 coords resulting from empty fields, and remove some unwanted properties that were stuck in during the xml export.
-cat "$temp/sheet.json"  | perl -pe "s/\[ 0\.0, 0\.0 \]/\"\"/g"  | jq '.' | \
-   grep -v -e \"id\": \
-   -e \"title\": \
-   -e \"title_type\": \
-   -e \"updated\": \
-   -e \"category_scheme\": \
-   -e \"category_term\": \
-   -e \"content\": \
-   -e \"content_type\": \
-   -e \"link_rel\": \
-   -e \"link_type\": \
-   -e \"link_href\": \
-   > "$temp/temples.json"
+# make sure file exists or else something went wrong
+if [ -s "$temp/sheet.json" ]
+then
+	cat "$temp/sheet.json"  | perl -pe "s/\[ 0\.0, 0\.0 \]/\"\"/g"  | jq '.' | \
+	   grep -v -e \"id\": \
+	   -e \"title\": \
+	   -e \"title_type\": \
+	   -e \"updated\": \
+	   -e \"category_scheme\": \
+	   -e \"category_term\": \
+	   -e \"content\": \
+	   -e \"content_type\": \
+	   -e \"link_rel\": \
+	   -e \"link_type\": \
+	   -e \"link_href\": \
+	   > "$temp/temples.json"
 
-# Get rid of last bit of extra stuff from the xml
-jq '{type: .type, features: .features}' "$temp/temples.json" > "$dest/temples.json"
+	# Get rid of last bit of extra stuff from the xml
+	jq '{type: .type, features: .features}' "$temp/temples.json" > "$dest/temples.json"
 
-# For mapping, save a second copy with only entries including coordinates
-head -n 3 "$dest/temples.json" > "$temp/temples.json"
-text=$(jq -c '.features[] | select (.geometry.coordinates != "")' "$dest/temples.json" | tr '\n' ',')
-text="$text ]}"
-echo $(echo $text | sed 's/, \]\}/\]\}/') >> "$temp/temples.json"
-jq '.' "$temp/temples.json" > "$dest/maps/temples.json"
+	# For mapping, save a second copy with only entries including coordinates
+	head -n 3 "$dest/temples.json" > "$temp/temples.json"
+	text=$(jq -c '.features[] | select (.geometry.coordinates != "")' "$dest/temples.json" | tr '\n' ',')
+	text="$text ]}"
+	echo $(echo $text | sed 's/, \]\}/\]\}/') >> "$temp/temples.json"
+	jq '.' "$temp/temples.json" > "$dest/maps/temples.json"
+
+else
+  echo "$(date): There was a problem with the json file." 1>&2
+fi
