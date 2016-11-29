0f2b5b523f56c03ea0c51c647f6f8438fccaacb8
John Muccigrosso <john@muccigrosso.org>
2016-11-29 11:07:14 -0500

    Updated to remove jq
    Details here:
    https://groups.google.com/forum/#!topic/pandoc-discuss/mp0PoYmWtGk

--- scripts/bib2html.sh
+++ scripts/bib2html.sh
@@ -6,16 +6,16 @@
 me=$(whoami)
 dest="/Users/$me/Documents/github/local/temples"
 
-# Path to include homebrew stuff like jq for running via launchctl
+# Path to include homebrew stuff like pandoc for running via launchctl
 export PATH="/usr/local/bin:$PATH"
 
-# Extract the article IDs from the json file and massage them into the right form.
-refs=$(jq --raw-output '.[].id' "$dest/temple_bib.json" | sed -e 's/^/@/' | perl -pe 's/\n/, /')
+# Convert the json file to yaml for pandoc-citeproc.
+refs=$(pandoc-citeproc -y "$dest/temple_bib.json")
 
-# Pad them out so that they're YAML with a 'nocite' metadata field
-# and send them to pandoc for conversion to HTML which gets written to a file
-echo "---
+# Add the 'nocite' metadata field & send them to pandoc for conversion to an HTML file
+echo "$refs
+---
 nocite: |
-  $refs
+  '@*'
 ...
-" | pandoc --filter pandoc-citeproc --bibliography="$dest/temple_bib.json" -o "$dest/temple_bib.html"
+" | pandoc --filter pandoc-citeproc -s -o "$dest/temple_bib.html"
