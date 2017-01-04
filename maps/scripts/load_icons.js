523b348698fb4da42f08a67d96eba6252f19e3dc
John Muccigrosso <john@muccigrosso.org>
2017-01-04 11:43:15 -0500

    Deleting space in URL

--- maps/scripts/load_icons.js
+++ maps/scripts/load_icons.js
@@ -43,7 +43,7 @@ var blueIcon = new L.Icon(
 var redIcon = new L.Icon(
 {
     iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
-    shadowUrl: 'https: //cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
     iconSize: [25, 41],
     iconAnchor: [12, 41],
     popupAnchor: [1, -34],
