e744f61c7ea0460b1fd6cf6bbae870dc576e46f6
John Muccigrosso <john@muccigrosso.org>
2017-03-06 12:57:55 -0500

    No spaces in header line.

--- scripts/json2csv.sh
+++ scripts/json2csv.sh
@@ -12,7 +12,7 @@ newfile="$dest/temples.csv"
 export PATH="/usr/local/bin:$PATH"
 
 # All the column names. Should be possible to do this automatically
-header='"name", "id", "type", "location", "city", "country", "orientation", "date", "startdateearly", "startdatelate", "century", "sex", "dedicationday", "deitytype", "extant", "source", "meetings", "blake", "coarellicm", "platnerandashby", "richardson", "stamper", "ziolkowski", "note", "vici.org", "pleiades", "dare", "arachne", "wikipedia", "wikidata", "longitude", "latitude"'
+header='"name","id","type","location","city","country","orientation","date","startdateearly","startdatelate","century","sex","dedicationday","deitytype","extant","source","meetings","blake","coarellicm","platnerandashby","richardson","stamper","ziolkowski","note","vici.org","pleiades","dare","arachne","wikipedia","wikidata","longitude","latitude"'
 
 jq '.features[]' $dest/temples.json | jq '[ .properties.name, .properties.id, .properties.type, .properties.location, .properties.city, .properties.country, .properties.orientation, .properties.date, .properties.startdateearly, .properties.startdatelate, .properties.century, .properties.sex, .properties.dedicationday, .properties.deitytype, .properties.extant, .properties.source, .properties.meetings, .properties.blake, .properties.coarellicm, .properties.platnerandashby, .properties.richardson, .properties.stamper, .properties.ziolkowski, .properties.note, .properties."vici.org", .properties.pleiades, .properties.dare, .properties.arachne, .properties.wikipedia, .properties.wikidata,  .geometry.coordinates[0], .geometry.coordinates[1] ]' | jq -r @csv > "$temp/csv.csv"
 
