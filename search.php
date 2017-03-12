6950e06a61e00f71d2db3dfe159706032081d2d1
John Muccigrosso <john@muccigrosso.org>
2017-03-12 15:20:59 -0400

    Initial commit

---
+++ search.php
@@ -0,0 +1,40 @@
+﻿<?php
+
+$servername = "localhost";
+$username = "romerese_temples";
+$dbname = "romerese_temples";
+$password = trim(file_get_contents("forbidden/pw.txt"));
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	$sql = 'SELECT * from temples';
+
+// Output column names as header row first
+	$rs = $conn->query($sql . ' LIMIT 0');
+	for ($i = 0; $i < $rs->columnCount(); $i++) {
+		$col = $rs->getColumnMeta($i);
+		$columns[] = $col['name'];
+	}
+	$string=implode(",",$columns);
+	echo $string . "\n";
+
+// Ouput records as csv
+    $stmt = $conn->prepare($sql); 
+    $stmt->execute();
+
+	$result = $stmt->fetchAll(PDO::FETCH_ASSOC); 
+	$out = fopen('php://output', 'w');
+	foreach ($result as $field) {
+		fputcsv($out, $field);
+		}
+	fclose($out);
+
+	$conn = null;
+}
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+
+?>
