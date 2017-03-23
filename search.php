c896347b16d8d0f3d1158dd8eb4c55c44b8b78e4
John Muccigrosso <john@muccigrosso.org>
2017-03-23 14:57:46 -0400

    Search by field value

--- search.php
+++ search.php
@@ -1,5 +1,13 @@
 ﻿<?php
 
+// Clean the URI
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
 $servername = "localhost";
 $username = "romerese_temples";
 $dbname = "romerese_temples";
@@ -11,26 +19,49 @@ try {
 
 	$sql = 'SELECT * from temples';
 
-// Output column names as header row first
+	$queryString = $_GET;
+	
+// If the URI contains some _GET string, assume it's valid and see what happens.
+if ($queryString != []) {
+//  Turn it into a sql request
+	$count = count($queryString);
+	$counter = 0;
+	$sql = $sql . " WHERE ";
+	foreach($queryString as $x => $x_value) {
+		$sql = $sql . test_input($x) . ' LIKE "%' . test_input($x_value) . '%"';
+		$counter++;
+		if ($counter < $count) {
+		    $sql = $sql . " AND ";
+		}
+	}
+}
+
+// Ouput records as csv
+    $stmt = $conn->prepare($sql); 
+    $stmt->execute();
+	$result = $stmt->fetchAll(PDO::FETCH_ASSOC); 
+
+// Make sure there were some results before doing a lot of work
+if ( count($result) > 0 ) {
+// First output column names as header row
 	$rs = $conn->query($sql . ' LIMIT 0');
 	for ($i = 0; $i < $rs->columnCount(); $i++) {
 		$col = $rs->getColumnMeta($i);
 		$columns[] = $col['name'];
 	}
-	$string=implode(",",$columns);
-	echo $string . "\n";
+	$headers=implode(",",$columns);
 
-// Ouput records as csv
-    $stmt = $conn->prepare($sql); 
-    $stmt->execute();
-
-	$result = $stmt->fetchAll(PDO::FETCH_ASSOC); 
-	$out = fopen('php://output', 'w');
+// Then output results
+    $out = fopen('php://output', 'w');
+	echo $headers . "\n";
 	foreach ($result as $field) {
 		fputcsv($out, $field);
 		}
 	fclose($out);
-
+}
+else {
+    echo "No records found matching query.";
+}
 	$conn = null;
 }
 catch(PDOException $e) {
