7624766043960f0cfa1cd90840314be43d27fb99
John Muccigrosso <john@muccigrosso.org>
2017-04-01 13:10:28 -0400

    Initial commit

---
+++ scripts/git_commmit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Commits some files to github.
+
+# Set up some variables
+me=$(whoami)
+dest="/Users/$me/Documents/github/local/temples"
+
+# Commit the various temple-db output files to git if they've changed
+gitList=$(cd "$dest"; git status -s)
+
+for f in temples.json temples.csv maps/temples.json maps/temples.js
+do
+	gitItem=$(echo "$gitList" | grep -e '^ M $f$')
+	if [ ${#gitItem} -ne 0 ] 
+		then echo "$(date +%Y-%m-%d\ %H:%M:%S) $f pushed to github" 1>&2
+		cd "$dest"; git commit -m 'Regular update' $f
+	fi
+done
