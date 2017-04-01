8e8cb8c6d2715280922c3e1b547f344b766c640e
John Muccigrosso <john@muccigrosso.org>
2017-04-01 14:15:20 -0400

    Always write to log.

--- scripts/git_commit.sh
+++ scripts/git_commit.sh
@@ -5,6 +5,7 @@
 # Set up some variables
 me=$(whoami)
 dest="/Users/$me/Documents/github/local/temples"
+counter=0
 
 # Commit the various temple-db output files to git if they've changed
 gitList=$(cd "$dest"; git status -s)
@@ -15,5 +16,8 @@ do
 	if [ ${#gitItem} -ne 0 ] 
 		then echo "$(date +%Y-%m-%d\ %H:%M:%S) $f pushed to github" 1>&2
 		cd "$dest"; git commit -m 'Regular update' $f
+		((counter++))
 	fi
 done
+
+echo "$(date +%Y-%m-%d\ %H:%M:%S) $counter temple-db changes pushed to github." 1>&2
