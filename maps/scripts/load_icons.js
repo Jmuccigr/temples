9c9d27edcc556a102214f6afb72009733e805146
John Muccigrosso <john@muccigrosso.org>
2017-04-02 11:36:57 -0400

    More icons

--- maps/scripts/load_icons.js
+++ maps/scripts/load_icons.js
@@ -20,6 +20,26 @@ var greenIcon = new L.Icon(
     shadowSize: [41, 41]
 });
 
+var yellowIcon = new L.Icon(
+{
+    iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-yellow.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.3/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+
+var orangeIcon = new L.Icon(
+{
+    iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-orange.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.3/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+
 var greyIcon = new L.Icon(
 {
     iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-grey.png',
