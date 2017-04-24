6decbacf5c5f7ab36988f63c97ff58b79b99bb06
John Muccigrosso <john@muccigrosso.org>
2017-04-23 20:47:00 -0400

    Fixing pw path

--- maps/scripts_for_sql/json.php
+++ maps/scripts_for_sql/json.php
@@ -9,7 +9,7 @@
 $servername = "localhost";
 $username = "romerese_temples";
 $dbname = "romerese_temples";
-$password = trim(file_get_contents("forbidden/pw.txt"));
+$password = trim(file_get_contents("../../forbidden/pw.txt"));
 
 # Connect to SQLite database
 $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
