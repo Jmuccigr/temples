f458dad55cb85bbadf24d78f3b2422fc58720baa
John Muccigrosso <john@muccigrosso.org>
2017-05-03 10:25:09 -0400

    Removing deleted fields

--- scripts/json2csv.sh
+++ scripts/json2csv.sh
@@ -12,9 +12,9 @@ newfile="$dest/temples.csv"
 export PATH="/usr/local/bin:$PATH"
 
 # All the column names. Should be possible to do this automatically
-header='"name","id","dedicatee","type","location","city","country","orientation","date","startdateearly","startdatelate","century","sex","dedicationday","deitytype","extant","source","meetings","blake","coarellicm","platnerandashby","richardson","stamper","ziolkowski","note","vici.org","pleiades","dare","arachne","wikipedia","wikidata","longitude","latitude"'
+header='"name","id","dedicatee","type","location","city","country","orientation","date","startdateearly","startdatelate","century","sex","dedicationday","deitytype","extant","source","meetings","note","vici.org","pleiades","dare","arachne","wikipedia","wikidata","longitude","latitude"'
 
-jq '.features[]' $dest/temples.json | jq '[ .properties.name, .properties.id, .properties.dedicatee, .properties.type, .properties.location, .properties.city, .properties.country, .properties.orientation, .properties.date, .properties.startdateearly, .properties.startdatelate, .properties.century, .properties.sex, .properties.dedicationday, .properties.deitytype, .properties.extant, .properties.source, .properties.meetings, .properties.blake, .properties.coarellicm, .properties.platnerandashby, .properties.richardson, .properties.stamper, .properties.ziolkowski, .properties.note, .properties."vici.org", .properties.pleiades, .properties.dare, .properties.arachne, .properties.wikipedia, .properties.wikidata,  .geometry.coordinates[0], .geometry.coordinates[1] ]' | jq -r @csv > "$temp/csv.csv"
+jq '.features[]' $dest/temples.json | jq '[ .properties.name, .properties.id, .properties.dedicatee, .properties.type, .properties.location, .properties.city, .properties.country, .properties.orientation, .properties.date, .properties.startdateearly, .properties.startdatelate, .properties.century, .properties.sex, .properties.dedicationday, .properties.deitytype, .properties.extant, .properties.source, .properties.meetings, .properties.note, .properties."vici.org", .properties.pleiades, .properties.dare, .properties.arachne, .properties.wikipedia, .properties.wikidata,  .geometry.coordinates[0], .geometry.coordinates[1] ]' | jq -r @csv > "$temp/csv.csv"
 
 echo $header > $newfile
 cat $temp/csv.csv >> $newfile
