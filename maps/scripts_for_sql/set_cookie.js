c1e8e87523b7faecd5706caa261e4765b1b45914
John Muccigrosso <john@muccigrosso.org>
2017-05-06 16:58:13 -0400

    New div name; remove counter

--- maps/scripts_for_sql/set_cookie.js
+++ maps/scripts_for_sql/set_cookie.js
@@ -10,15 +10,14 @@ function setCookie(name, value, days) {
         expires = "";
     }
     document.cookie = escape(name) + "=" + escape(value) + expires + "; path=/";
-    document.getElementById("p1").innerHTML = 'Current query:' + value;
+    document.getElementById("side").innerHTML = 'Current query:' + value;
     // Send change event to menu that selects overlay layers so it updates the map
     var event = new Event('change');
     var value = document.getElementById("mymenu").options[selectmenu.selectedIndex].value;
-	if (value == 'nothing' & runCounter > 0) {
+	if (value == 'nothing' && days != '-1') {
 		alert ("Don't forget to select a map variable!")
 	}
 	else {
-	runCounter++;
 	selectmenu.dispatchEvent(event);
 	}
 }
