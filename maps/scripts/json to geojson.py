52b1d42c4e547444da2db1c4bc7154e6dab2c86d
John Muccigrosso <john@muccigrosso.org>
2017-05-10 15:44:56 -0400

    Reformat

--- maps/scripts/json to geojson.py
+++ maps/scripts/json to geojson.py
@@ -3,16 +3,23 @@
 
 def convert_json(items):
     import json
-    return json.dumps({ "type": "FeatureCollection",
-                        "features": [ 
-                                        {"type": "Feature",
-                                         "geometry": { "type": "Point",
-                                                       "coordinates": [ feature['longitude'],
-                                                                        feature['latitude']]},
-                                         "properties": { key: value 
-                                                         for key, value in feature.items()
-                                                         if key not in ('latitude', 'longitude') }
-                                         } 
-                                     for feature in json.loads(items)
-                                    ]
-                       })
+return json.dumps({
+    "type": "FeatureCollection",
+    "features": [{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [feature['longitude'],
+                    feature['latitude']
+                ]
+            },
+            "properties": {
+                key: value
+                for key,
+                value in feature.items()
+                if key not in ('latitude', 'longitude')
+            }
+        }
+        for feature in json.loads(items)
+    ]
+})
