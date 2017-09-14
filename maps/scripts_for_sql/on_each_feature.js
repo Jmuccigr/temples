539d9774c8cb38346244b3190d76d9abdf1349b6
John Muccigrosso <john@muccigrosso.org>
2017-09-14 11:25:46 -0400

    Wikipedia; space clean-up

--- maps/scripts_for_sql/on_each_feature.js
+++ maps/scripts_for_sql/on_each_feature.js
@@ -12,50 +12,54 @@ function onEachFeature(feature, layer) {
                 text = '<h4>Temple of ' + feature.properties.name + '</h4><span style="font-size:small"><ul>';
                 if (feature.properties.id != '') {
                     text = text + '<li>ID: ' + feature.properties.id + '</li>';
-                    }                
+                    }
                 if (feature.properties.dedicatee != '') {
                     text = text + '<li>Dedicated to ' + feature.properties.dedicatee.replace (/,/g, ' and ') + '</li>';
-                    }                
+                    }
                 if (feature.properties.location != '' || feature.properties.city != '' || feature.properties.country != '') {
                     loc = '<li>Location: ';
-                    if (feature.properties.location != '') { 
+                    if (feature.properties.location != '') {
 						loc = loc + feature.properties.location;
 						l = true;
 						}
                     if (feature.properties.city != '') {
-						if (l) { loc = loc +  ' in '; } 
-						loc = loc + feature.properties.city; 
+						if (l) { loc = loc +  ' in ';}
+						loc = loc + feature.properties.city;
 						}
-                    if (feature.properties.country != '') { 
+                    if (feature.properties.country != '') {
 						if (loc.length > 14 ) {loc = loc +  ', ' }
-						loc = loc + feature.properties.country; 
+						loc = loc + feature.properties.country;
 						}
                     text = text + loc + '</li>';
-                    }                
+                    }
                 // Get the other database info and put it on one line
                 if (feature.properties.pleiades != '') {
                     otherDB = otherDB + '<a target="_blank" href="https://pleiades.stoa.org/places/' + feature.properties.pleiades + '">Pleiades</a>';
-                    }                
+                    }
                 if (feature.properties.dare != '') {
                     if (otherDB.length > 4) { otherDB = otherDB + ', ' };
                     otherDB = otherDB + '<a target="_blank" href="http://dare.ht.lu.se/places/' + feature.properties.dare + '">DARE</a>';
-                    }                
+                    }
                 if (feature.properties["vici.org"] != '') {
                     if (otherDB.length > 4) { otherDB = otherDB + ', ' };
                     otherDB = otherDB + '<a target="_blank" href="https://vici.org/vici/' + feature.properties["vici.org"] + '">Vici.org</a>';
-                    }                
+                    }
                 if (feature.properties.arachne != '') {
                     if (otherDB.length > 4) { otherDB = otherDB + ', ' };
                     otherDB = otherDB + '<a target="_blank" href="https://arachne.dainst.org/entity/' + feature.properties.arachne + '">Arachne</a>';
-                    }                
+                    }
                 if (feature.properties.wikidata != '') {
                     if (otherDB.length > 4) { otherDB = otherDB + ', ' };
                     otherDB = otherDB + '<a target="_blank" href="https://www.wikidata.org/wiki/' + feature.properties.wikidata + '">WikiData</a>';
-                    }            
+                    }
+                if (feature.properties.wikipedia != '') {
+                    if (otherDB.length > 4) { otherDB = otherDB + ', ' };
+                    otherDB = otherDB + '<a target="_blank" href="https://en.wikipedia.org/wiki/' + feature.properties.wikipedia + '">Wikipedia</a>';
+                    }
                 if (otherDB.length == 4) { otherDB = '' } else { otherDB = otherDB + '</li>' }
                 if (feature.properties.cite != '') {
                     cites = '<li>Cited in<ol><li>' + feature.properties.cite + '</li></ol>';
-                    }                
+                    }
                 document.getElementById("side").innerHTML = text + otherDB + cites +
 				'<br><span style="font-size:x-small; align=center;" onclick="loadNavigation()"><br>(Restore navigation instructions.)</span>';
             }
