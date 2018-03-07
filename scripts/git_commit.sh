0fc574628495d2d28daf87c7c8d327b302885c56
John Muccigrosso <john@muccigrosso.org>
2018-03-07 14:15:58 -0500

    Don't automate git push for temple files

--- scripts/git_commit.sh
+++ scripts/git_commit.sh
@@ -10,10 +10,12 @@ counter=0
 # Commit the various temple-db output files to git if they've changed
 gitList=$(cd "$dest"; git status -s)
 
-for f in temples.json temples.csv maps/temples.json maps/temples.js sheet.csv citations.csv bibliography.csv temple_bib.html temple_bib.json
+# Leaving out temples.json temples.csv maps/temples.json maps/temples.js
+# so that they can have more meaningful commit messages
+for f in sheet.csv citations.csv bibliography.csv temple_bib.html temple_bib.json
 do
 	gitItem=$(echo "$gitList" | grep -e " M $f")
-	if [ ${#gitItem} -ne 0 ] 
+	if [ ${#gitItem} -ne 0 ]
 		then if [ -s "$dest/$f" ]
 		then
     		echo "$(date +%Y-%m-%d\ %H:%M:%S) $f pushed to github" 1>&2
