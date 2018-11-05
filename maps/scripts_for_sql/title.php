b0a9d5235504b3af968b9ad13f0bef56e3322313
John Muccigrosso <john@muccigrosso.org>
2018-11-04 21:36:07 -0800

    Fix cookie problem

--- maps/scripts_for_sql/title.php
+++ maps/scripts_for_sql/title.php
@@ -5,8 +5,7 @@ $servername = "localhost";
 $username = "romerese_temples";
 $dbname = "romerese_temples";
 $password = trim(file_get_contents("../forbidden/pw.txt"));
-// sqlquery cookie will have "ID=#######" if this is called from items view
-$sql = 'select name, city, country FROM temples WHERE ' . $_COOKIE["sqlquery"];
+$sql = 'select name, city, country FROM temples WHERE ID = ' . basename($_SERVER[REQUEST_URI]);
 
 # Connect to SQLite database
 $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
@@ -15,13 +14,13 @@ try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$stmt = $conn->query($sql);
-// Should be only one, but loop anyway
-while ($row = $stmt->fetch())
-	{
-	$text = '        <title>' . $row['name'] . ' (' . trim($row['city'] . ' ' . $row['country']) . ')</title>' . "\n";
+	$stmt = $conn->query($sql);
+	// Should be only one, but loop anyway
+	while ($row = $stmt->fetch())
+		{
+		$text = '        <title>' . $row['name'] . ' (' . trim($row['city'] . ' ' . $row['country']) . ')</title>' . "\n";
+		}
 	}
-    }
 catch(PDOException $e) {
     echo "Error: " . $e->getMessage();
 }
