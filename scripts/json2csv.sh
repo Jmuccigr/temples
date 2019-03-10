4a3fcb3e52221817ae303e21aa6f5606ee7b3d02
John Muccigrosso <john@muccigrosso.org>
2019-03-10 16:03:53 -0700

    Added fields; automated reading field titles using jq

--- scripts/json2csv.sh
+++ scripts/json2csv.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Converts google-derived json into a nice csv
-
 # Set up some variables
 me=$(whoami)
 dest="/Users/$me/Documents/github/local/temples"
@@ -11,11 +10,12 @@ newfile="$dest/temples.csv"
 # Path to include homebrew jq for running via launchctl
 export PATH="/usr/local/bin:$PATH"
 
-# All the column names. Should be possible to do this automatically
-# Removing this for ease of import into SQL database
-# header='"id","name","dedicatee","type","location","city","country","orientation","compass","date","startdateearly","startdatelate","century","sex","dedicationday","deitytype","culture","extant","source","meetings","note","vici","pleiades","dare","arachne","livius","trismegistos","digitalromanforum", "digitalesforumromanum","wikipedia","wikidata","ads","cona","topostext","longitude","latitude"'
+# Get all the column names for properties except for the record-keeping one, "checked".
+header=(`jq '.features[0].properties' "$dest/temples.json" | jq 'keys_unsorted[]' | grep -v "checked" | tr -d '"'`)
+proplist=""
+for i in ${header[*]}; do
+  proplist="$proplist .properties.$i,"
+done
 
-jq '.features[]' "$dest/temples.json" | jq '[ .properties.id, .properties.name, .properties.dedicatee, .properties.type, .properties.location, .properties.city, .properties.country, .properties.orientation, .properties.compass, .properties.date, .properties.startdateearly, .properties.startdatelate, .properties.century, .properties.sex, .properties.dedicationday, .properties.deitytype, .properties.culture, .properties.extant, .properties.source, .properties.meetings, .properties.note, .properties.vici, .properties.pleiades, .properties.dare, .properties.arachne, .properties.livius, .properties.wikipedia, .properties.wikidata, .properties.digitalromanforum, .properties.digitalesforumromanum, .properties.trismegistos, .properties.ads, .properties.cona, .properties.topostext, .properties.url, .geometry.coordinates[0], .geometry.coordinates[1] ]' | jq -r @csv > "$newfile"
-
-# echo $header > $newfile
-# cat $temp/csv.csv > "$newfile"
+# Re-assemble the CVS using jq, then converting the escaped quotation marks
+jq '.features[]' "$dest/temples.json" | jq "[ $proplist .geometry.coordinates[0], .geometry.coordinates[1] ]" | jq -r @csv | perl -pe 's/([ a-zA-Z])""/\1\\"/g' > "$dest/temples.csv"
