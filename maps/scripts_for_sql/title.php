9300c3ead7e0325c483a9051c844170eba0b1d63
John Muccigrosso <john@muccigrosso.org>
2019-11-19 00:02:31 +0100

    Pull more info to allow unique titles

--- maps/scripts_for_sql/title.php
+++ maps/scripts_for_sql/title.php
@@ -5,7 +5,7 @@ $servername = "localhost";
 $username = "romerese_temples";
 $dbname = "romerese_temples";
 $password = trim(file_get_contents("../forbidden/pw.txt"));
-$sql = 'select name, city, country FROM temples WHERE ID = ' . basename($_SERVER[REQUEST_URI]);
+$sql = 'select ID, name, location, city, country FROM temples WHERE ID = ' . basename($_SERVER[REQUEST_URI]);
 
 # Connect to SQLite database
 // $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
@@ -18,12 +18,16 @@ try {
 	// Should be only one, but loop anyway
 	while ($row = $stmt->fetch())
 		{
-		$text = '        <title>' . $row['name'] . ' (' . trim($row['city'] . ' ' . $row['country']) . ')</title>' . "\n";
+		$text = '        <title>' . $row['name'] . ' (' . trim($row['location'] . ' - ' . $row['city'] . ', ' . $row['country']) . '): ' . $row['ID'] . '</title>' . "\n";
+		$text = str_replace('(- ', '(', $text);
+		$text = str_replace(' - ,', ',', $text);
+		$text = str_replace(' (,)', '', $text);
 		}
 	}
 catch(PDOException $e) {
     echo "Error: " . $e->getMessage();
 }
 $conn = null;
+
 echo $text;
 ?>
