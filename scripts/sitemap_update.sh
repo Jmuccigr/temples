f55d26b64a1a5b44a4c5c4c5679aedbdb1832224
John Muccigrosso <john@muccigrosso.org>
2019-11-24 13:16:58 +0100

    Check for changes before updating

--- scripts/sitemap_update.sh
+++ scripts/sitemap_update.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-# Updates sitemap file and pushes it to site
+# Updates sitemap file
 # Set up some variables
 me=$(whoami)
 src="/Users/$me/Documents/github/local/temples"
 
 # Update sitemap file
-grep romeresearchgroup.org/items/[0-9] $src/temples.ttl | perl -pe 's/<(.*)>.*/\1/' > /Users/$me/Documents/github/local/temples/sitemap.txt
+grep romeresearchgroup.org/items/[0-9] $src/temples.ttl | perl -pe 's/<(.*)>.*/\1/' > $src/sitemap2.txt
+
+if [ -s "$src/sitemap2.txt" ]
+then
+	csvdiff=$(diff "$src/sitemap.txt" "$src/sitemap2.txt")
+	if [ ${#csvdiff} -eq 0 ]
+	   then
+	   echo "$(date +%Y-%m-%d\ %H:%M:%S) No change to sitemap." 1>&2
+	   rm $src/sitemap2.txt
+	   exit 0
+	else
+	   mv $src/sitemap2.txt $src/sitemap.txt
+	fi
+else
+    echo "$(date +%Y-%m-%d\ %H:%M:%S) Error making csv file." 1>&2
+fi
 
