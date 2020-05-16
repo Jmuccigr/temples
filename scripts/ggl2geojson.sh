026477daaa0435b76d7602edc2d5ff8a6f2306e5
John Muccigrosso <john@muccigrosso.org>
2020-05-16 19:44:11 +0200

    Update to use google's v4 API

--- scripts/ggl2geojson.sh
+++ scripts/ggl2geojson.sh
@@ -10,7 +10,7 @@ dest="/Users/$me/Documents/github/local/temples"
 temp=$(echo $TMPDIR | sed 's:/$::')
 
 # Path to include homebrew stuff like jq for running via launchctl
-export PATH="/usr/local/bin:/usr/local/opt/gdal2/bin:$PATH"
+export PATH="/usr/local/bin:$PATH"
 
 # Quick check that ogr2ogr exists
 check=$(which ogr2ogr)
@@ -28,10 +28,50 @@ if [ ${#json} -lt 100 ]
    exit 0
 fi
 
+# Make sure response is also valid json and convert it to csv
+echo "$json" | json_verify -q
+if [ "$?" == 0 ]
+then
+    json=`echo "$json" | jq -r '.values[] | @csv'`
+else
+   echo "$(date +%Y-%m-%d\ %H:%M:%S) Invalid temple json received from google" 1>&2
+   exit 0
+fi
+
+# Fix headers so that they're like the old method:
+# 		= no caps or spaces
+# Delete this when I decide there's no harm
+newheaders=`echo "$json" | head -n 1 | tr -d " " | tr [:upper:] [:lower:]`
+newsheet=`echo "$json" | tail -n +2`
+
+json=`echo -e $newheaders"\n""$newsheet"`
+
+# Make sure something has changed or else exit
+if [ -s "$dest/sheet.csv" ]
+then
+	jsonsum=`echo "$json" | md5`
+	filesum=`md5 -q "$dest/sheet.csv"`
+	if [ $jsonsum = $filesum ]
+	then
+	   echo "$(date +%Y-%m-%d\ %H:%M:%S) No change to temple data." 1>&2
+	   exit 0
+	fi
+fi
+
+# Save a copy as a csv file
+echo "$json" > "$dest/sheet.csv"
+if [ ! $? ]
+then
+  echo "$(date +%Y-%m-%d\ %H:%M:%S) There was a problem saving the temple csv file." 1>&2
+  exit 0
+fi
+
+# Convert to bad geojson
+
 # Create the vrt file for the conversion from csv to geojson
 text="      <OGRVRTDataSource>"
 text="$text     <OGRVRTLayer name=\"sheet\">"
-text="$text         <SrcDataSource>$temp/sheet.csv</SrcDataSource>"
+text="$text         <SrcDataSource>$dest/sheet.csv</SrcDataSource>"
 text="$text         <GeometryType>wkbPoint</GeometryType>"
 text="$text         <LayerSRS>WGS84</LayerSRS>"
 text="$text         <GeometryField encoding=\"PointFromColumns\" x=\"Longitude\" y=\"Latitude\"/>"
@@ -40,50 +80,6 @@ text="$text </OGRVRTDataSource>"
 
 echo $text > "$temp/sheet.vrt"
 
-# Convert json to csv
-# perl bit adds a date stamp to the warning output for log file
-rm "$temp/sheet"*.csv 2>/dev/null
-echo $json | jq -r '.values[] | @csv' > "$temp/sheet.csv" | perl -p -MPOSIX -e 'BEGIN {$|=1} $_ = strftime("%Y-%m-%d %T ", localtime) . $_' 1>&2
-
-# Make sure the csv has been created and is big enough or else exit
-if  [ ! -s "$temp/sheet.csv" ]
-then
-   echo "$(date +%Y-%m-%d\ %H:%M:%S) sheet.csv not created." 1>&2
-   echo "Error with sheet.csv creation" | mail -s "Temples problem: data" $me
-   exit 0
-else
-    if  [ `stat -f%z "$temp/sheet.csv"` -lt 50000 ]
-    then
-		echo "$(date +%Y-%m-%d\ %H:%M:%S) sheet.csv is too small." 1>&2
-		echo "Too small sheet.csv" | mail -s "Temples problem: data" $me
-		exit 0
-	fi
-fi
-
-# Fix headers so that they're like the old method:
-# 		= no caps or spaces
-# Delete this when I decide there's no harm
-newheaders=`head -n 1 "$temp/sheet.csv" | tr -d " " | tr [:upper:] [:lower:]`
-newsheet=`tail -n +2 "$temp/sheet.csv"`
-echo -e $newheaders"\n""$newsheet" > "$temp/sheet.csv"
-
-# Make sure something has changed or else exit
-if [ -s "$dest/sheet.csv" ]
-then
-	csvdiff=$(diff "$temp/sheet.csv" "$dest/sheet.csv")
-	if [ ${#csvdiff} -eq 0 ]
-	   then
-	   echo "$(date +%Y-%m-%d\ %H:%M:%S) No change to temple data." 1>&2
-	   exit 0
-	fi
-else
-    echo "$(date +%Y-%m-%d\ %H:%M:%S) No csv file in destination folder." 1>&2
-fi
-
-# Save a copy of the csv file
-cp "$temp/sheet.csv" "$dest/sheet.csv" 2>&1 | perl -p -MPOSIX -e 'BEGIN {$|=1} $_ = strftime("%Y-%m-%d %T ", localtime) . $_' 1>&2
-
-# Convert to bad geojson
 rm "$temp/sheet.json" 2>/dev/null
 ogr2ogr -addfields -skipfailures -f geojson "$temp/sheet.json" "$temp/sheet.vrt"
 
