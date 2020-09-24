1d2781ea0d9fac59f83b134a0e42998c4903b22a
John Muccigrosso <john@muccigrosso.org>
2020-09-24 23:47:16 +0200

    Separate out the 8 compass points

--- maps/scripts_for_sql/function_layer_by_orientation.js
+++ maps/scripts_for_sql/function_layer_by_orientation.js
@@ -1,8 +1,15 @@
 ﻿function layer_by_orientation() {
 <!--  My datafile -->
 	$.getJSON("scripts/json.php", function (data) {
-        templesO.addData(data);
         templesN.addData(data);
+        templesNE.addData(data);
+        templesE.addData(data);
+        templesSE.addData(data);
+        templesS.addData(data);
+        templesSW.addData(data);
+        templesW.addData(data);
+        templesNW.addData(data);
+        templesUnknown.addData(data);
         allPoints = data.features;
     });
 
@@ -18,13 +25,20 @@
 	clearControls();
 
     // Define layers for the various kinds of monuments in temples.js
-    var orientations = new L.LayerGroup(),
-        none = new L.LayerGroup();
+    var north = new L.LayerGroup(),
+    	northeast = new L.LayerGroup(),
+    	east = new L.LayerGroup(),
+    	southeast = new L.LayerGroup(),
+    	south = new L.LayerGroup(),
+    	southwest = new L.LayerGroup(),
+    	west = new L.LayerGroup(),
+    	northwest = new L.LayerGroup(),
+        unknown = new L.LayerGroup();
 
     // Set the offset value for the tooltip
     oset = 0;
 
-   templesO = new L.geoJson(null, {
+   templesN = new L.geoJson(null, {
         onEachFeature: onEachFeature,
         pointToLayer: function (feature, latlng) {
             return L.marker(latlng, {
@@ -35,11 +49,116 @@
             })
         },
         filter: function (feature, layer) {
-            return (feature.properties.orientation != "");
+            return (feature.properties.compass === "N");
         }
-    }).addTo(orientations);
+    }).addTo(north);
 
-   templesN = new L.geoJson(null, {
+   templesNE = new L.geoJson(null, {
+        onEachFeature: onEachFeature,
+        pointToLayer: function (feature, latlng) {
+            return L.marker(latlng, {
+                icon: arrowIcon,
+                riseOnHover: true,
+                rotationAngle: feature.properties.orientation,
+                rotationOrigin: 'center center'
+            })
+        },
+        filter: function (feature, layer) {
+            return (feature.properties.compass === "NE");
+        }
+    }).addTo(northeast);
+
+   templesE = new L.geoJson(null, {
+        onEachFeature: onEachFeature,
+        pointToLayer: function (feature, latlng) {
+            return L.marker(latlng, {
+                icon: arrowIcon,
+                riseOnHover: true,
+                rotationAngle: feature.properties.orientation,
+                rotationOrigin: 'center center'
+            })
+        },
+        filter: function (feature, layer) {
+            return (feature.properties.compass === "E");
+        }
+    }).addTo(east);
+
+   templesSE = new L.geoJson(null, {
+        onEachFeature: onEachFeature,
+        pointToLayer: function (feature, latlng) {
+            return L.marker(latlng, {
+                icon: arrowIcon,
+                riseOnHover: true,
+                rotationAngle: feature.properties.orientation,
+                rotationOrigin: 'center center'
+            })
+        },
+        filter: function (feature, layer) {
+            return (feature.properties.compass === "SE");
+        }
+    }).addTo(southeast);
+
+   templesS = new L.geoJson(null, {
+        onEachFeature: onEachFeature,
+        pointToLayer: function (feature, latlng) {
+            return L.marker(latlng, {
+                icon: arrowIcon,
+                riseOnHover: true,
+                rotationAngle: feature.properties.orientation,
+                rotationOrigin: 'center center'
+            })
+        },
+        filter: function (feature, layer) {
+            return (feature.properties.compass === "S");
+        }
+    }).addTo(south);
+
+   templesSW = new L.geoJson(null, {
+        onEachFeature: onEachFeature,
+        pointToLayer: function (feature, latlng) {
+            return L.marker(latlng, {
+                icon: arrowIcon,
+                riseOnHover: true,
+                rotationAngle: feature.properties.orientation,
+                rotationOrigin: 'center center'
+            })
+        },
+        filter: function (feature, layer) {
+            return (feature.properties.compass === "SW");
+        }
+    }).addTo(southwest);
+
+   templesW = new L.geoJson(null, {
+        onEachFeature: onEachFeature,
+        pointToLayer: function (feature, latlng) {
+            return L.marker(latlng, {
+                icon: arrowIcon,
+                riseOnHover: true,
+                rotationAngle: feature.properties.orientation,
+                rotationOrigin: 'center center'
+            })
+        },
+        filter: function (feature, layer) {
+            return (feature.properties.compass === "W");
+        }
+    }).addTo(west);
+
+   templesNW = new L.geoJson(null, {
+        onEachFeature: onEachFeature,
+        pointToLayer: function (feature, latlng) {
+            return L.marker(latlng, {
+                icon: arrowIcon,
+                riseOnHover: true,
+                rotationAngle: feature.properties.orientation,
+                rotationOrigin: 'center center'
+            })
+        },
+        filter: function (feature, layer) {
+            return (feature.properties.compass === "NW");
+        }
+    }).addTo(northwest);
+
+   templesUnknown = new L.geoJson(null, {
 		onEachFeature: onEachFeature,
         pointToLayer: function (feature, latlng) {
             return L.marker(latlng, {
@@ -48,13 +167,20 @@
             })
         },
         filter: function (feature, layer) {
-            return (feature.properties.orientation === "");
+            return (feature.properties.compass === "");
         }
-    }).addTo(none);
+    }).addTo(unknown);
 
     var overlays = {
-        "Oriented": orientations,
-        "Unknown": none
+        "North": north,
+        "Northeast": northeast,
+        "East": east,
+        "Southeast": southeast,
+        "South": south,
+        "Southwest": southwest,
+        "West": west,
+        "Northwest": northwest,
+        "Unknown": unknown
     };
 
     // Make sure this is global so the control can be deleted.
@@ -67,6 +193,13 @@
     // First restore the map currently in use
     map.addLayer(currentMap);
     // Now add the overlays
-    map.addLayer(orientations);
-    map.addLayer(none);
+    map.addLayer(north);
+    map.addLayer(northeast);
+    map.addLayer(east);
+    map.addLayer(southeast);
+    map.addLayer(south);
+    map.addLayer(southwest);
+    map.addLayer(west);
+    map.addLayer(northwest);
+    map.addLayer(unknown);
 }
