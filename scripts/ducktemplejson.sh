235d3d2139cfba17425df1235f876c2d8b6537e3
John Muccigrosso <john@muccigrosso.org>
2020-12-24 00:57:16 -0500

    More detail in error

--- scripts/ducktemplejson.sh
+++ scripts/ducktemplejson.sh
@@ -12,7 +12,7 @@ export PATH="/usr/local/bin:$PATH"
 fsize=`stat -f%z "$src/pelagios.json"`
 if [ $fsize -lt 10000 ]
 then
-	echo "$(date +%Y-%m-%d\ %H:%M:%S) temples pelagios json file is too small" 1>&2
+	echo "$(date +%Y-%m-%d\ %H:%M:%S) temples pelagios json file is too small to upload" 1>&2
 	exit 0
 else
 	duck -r 4 -y -e upload --synchronize ftps://romerese@ftp.romeresearchgroup.org/public_html/items/pelagios.json "$src/pelagios.json"; if [ $? -eq 0 ]; then echo "$(date +%Y-%m-%d\ %H:%M:%S) temples json updated." 1>&2; else echo "$(date +%Y-%m-%d\ %H:%M:%S) temples json update failed!" 1>&2; fi
