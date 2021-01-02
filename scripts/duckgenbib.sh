7128e7996e461838921f10dcbd622592c25f5186
John Muccigrosso <john@muccigrosso.org>
2021-01-02 11:58:50 -0500

    Creating files

---
+++ scripts/duckgenbib.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Pushes updated general temple bibliography to site
+# Set up some variables
+me=$(whoami)
+src="/Users/$me/Documents/github/local/temples"
+
+# Path to include Cyberduck's duck command for running via launchctl
+export PATH="/usr/local/bin:$PATH"
+
+# If file is big enough, Upload via FTP and log success or failure
+fsize=`stat -f%z "$src/temple_bib_general.html"`
+if [ $fsize -lt 500 ]
+then
+	echo "$(date +%Y-%m-%d\ %H:%M:%S) general temple bibliography file is too small" 1>&2
+	exit 0
+else
+	duck -r 4 -y -e upload --synchronize ftps://romerese@ftp.romeresearchgroup.org/public_html/temples/temple_bib_general.html "$src/gen_temple_bib.html"; if [ $? -eq 0 ]; then echo "$(date +%Y-%m-%d\ %H:%M:%S) general temple bibliography updated." 1>&2; else echo "$(date +%Y-%m-%d\ %H:%M:%S) general temple bibliography update failed!" 1>&2; fi
+fi
