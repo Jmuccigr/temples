2e4de3798d906cb524efb4919fa4cee6751df470
John Muccigrosso <john@muccigrosso.org>
2021-05-02 13:57:15 -0400

    Include new circle icons

--- maps/scripts/load_icons.js
+++ maps/scripts/load_icons.js
@@ -117,3 +117,95 @@ var circleIcon = L.icon(
     iconUrl: '/icons/circle-marker_red.png',
     iconSize: [12, 12]
 });
+
+// Circles for uncertain places
+
+var blackCircleIcon = new L.Icon(
+{
+    iconUrl: '/icons/marker-icon-2x-black-circle.png',
+    shadowUrl: '/icons/marker-shadow-circle.png',
+    iconSize: [25, 25],
+    iconAnchor: [12, 13],
+    shadowSize: [34, 35],
+    shadowAnchor: [12, 22]
+});
+
+var whiteCircleIcon = new L.Icon(
+{
+    iconUrl: '/icons/marker-icon-2x-white-circle.png',
+    shadowUrl: '/icons/marker-shadow-circle.png',
+    iconSize: [25, 25],
+    iconAnchor: [12, 13],
+    shadowSize: [34, 35],
+    shadowAnchor: [12, 22]
+});
+
+var greenCircleIcon = new L.Icon(
+{
+    iconUrl: '/icons/marker-icon-2x-green-circle.png',
+    shadowUrl: '/icons/marker-shadow-circle.png',
+    iconSize: [25, 25],
+    iconAnchor: [12, 13],
+    shadowSize: [34, 35],
+    shadowAnchor: [12, 22]
+});
+
+var yellowCircleIcon = new L.Icon(
+{
+    iconUrl: '/icons/marker-icon-2x-yellow-circle.png',
+    shadowUrl: '/icons/marker-shadow-circle.png',
+    iconSize: [25, 25],
+    iconAnchor: [12, 13],
+    shadowSize: [34, 35],
+    shadowAnchor: [12, 22]
+});
+
+var orangeCircleIcon = new L.Icon(
+{
+    iconUrl: '/icons/marker-icon-2x-orange-circle.png',
+    shadowUrl: '/icons/marker-shadow-circle.png',
+    iconSize: [25, 25],
+    iconAnchor: [12, 13],
+    shadowSize: [34, 35],
+    shadowAnchor: [12, 22]
+});
+
+var greyCircleIcon = new L.Icon(
+{
+    iconUrl: '/icons/marker-icon-2x-grey-circle.png',
+    shadowUrl: '/icons/marker-shadow-circle.png',
+    iconSize: [25, 25],
+    iconAnchor: [12, 13],
+    shadowSize: [34, 35],
+    shadowAnchor: [12, 22]
+});
+
+var blueCircleIcon = new L.Icon(
+{
+    iconUrl: '/icons/marker-icon-2x-blue-circle.png',
+    shadowUrl: '/icons/marker-shadow-circle.png',
+    iconSize: [25, 25],
+    iconAnchor: [12, 13],
+    shadowSize: [34, 35],
+    shadowAnchor: [12, 22]
+});
+
+var redCircleIcon = new L.Icon(
+{
+    iconUrl: '/icons/marker-icon-2x-red-circle.png',
+    shadowUrl: '/icons/marker-shadow-circle.png',
+    iconSize: [25, 25],
+    iconAnchor: [12, 13],
+    shadowSize: [34, 35],
+    shadowAnchor: [12, 22]
+});
+
+var BandWCircleIcon = new L.Icon(
+{
+    iconUrl: '/icons/marker-icon-2x-b&w-circle.png',
+    shadowUrl: '/icons/marker-shadow-circle.png',
+    iconSize: [25, 25],
+    iconAnchor: [12, 13],
+    shadowSize: [34, 35],
+    shadowAnchor: [12, 22]
+});
