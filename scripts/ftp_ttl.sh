1eec0c9ab20beb726dae6733244bf57842bb5498
John Muccigrosso <john@muccigrosso.org>
2021-12-28 23:34:10 -0500

    Better error & setup

--- scripts/ftp_ttl.sh
+++ scripts/ftp_ttl.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-fname='/Users/john_muccigrosso/Documents/github/local/temples/htdocs-items/temples.ttl'
+# Set up some variables
+me=$(whoami)
+src="/Users/$me/Documents/github/local/temples"
+
+# Path to include Cyberduck's duck command for running via launchctl
+export PATH="/usr/local/bin:$PATH"
+
+fname="$src/htdocs-items/temples.ttl"
 
 # Make sure file exists before doing anything
 if [ -s $fname ]
-  then
-  /usr/local/bin/duck -r 4 -y -e upload --synchronize ftps://romerese@romeresearchgroup.org/public_html/items/ $fname
+then
+    duck -r 4 -y -e upload --synchronize ftps://romerese@romeresearchgroup.org/public_html/items/ $fname
+else
+    echo "$(date +%Y-%m-%d\ %H:%M:%S) ftp_ttl: the temples turtle file was empty or non-existent." 1>&2
 fi
 
