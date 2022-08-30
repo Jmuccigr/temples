20c1b3cd070150e8a238944c854ef11552c8b787
John Muccigrosso <john@muccigrosso.org>
2022-08-30 20:58:43 +0200

    New AWMC/CAWM map info

--- maps/scripts/load_basemaps.js
+++ maps/scripts/load_basemaps.js
@@ -1,4 +1,5 @@
 ﻿var token = 'pk.eyJ1IjoibXVjY2lncm9zc28iLCJhIjoiY2lxanZ2d2luMDBlaGdoajl2YWJtZ3I2ZSJ9.MASxkbE98-h55S5W7oM0xQ';
+//var token = 'pk.eyJ1IjoiaXNhd255dSIsImEiOiJBWEh1dUZZIn0.SiiexWxHHESIegSmW8wedQ';
 
 var mbAttr = '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
     mbUrl = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=' + token;
@@ -6,8 +7,9 @@ var mbAttr = '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a h
 var dareAttr = 'Powered by <a href="http://leafletjs.com/">Leaflet</a>. Map base: <a href="https://dh.gu.se/dare/" title="Digital Atlas of the Roman Empire, Centre for Digital Humanities, University of Gothenburg, Sweden">DARE</a> (CC BY 4.0).',
     dareUrl = 'https://dh.gu.se/tiles/imperium/{z}/{x}/{y}.png';
 
-var awmcAttr = 'Powered by <a href="http://leafletjs.com/">Leaflet</a> and <a href="https://www.mapbox.com/">Mapbox</a>. Map base by <a title="Ancient World Mapping Center (UNC-CH)" href="http://awmc.unc.edu">AWMC</a>, 2014 (cc-by-4.0).',
-    awmcUrl = 'https://api.tiles.mapbox.com/v4/isawnyu.map-knmctlkh/{z}/{x}/{y}.png?access_token=' + token;
+var cawmAttr = 'Powered by <a href="http://leafletjs.com/">Leaflet</a> and <a href="https://www.mapbox.com/">Mapbox</a>. Map base by <a title="Consortium of Ancient World Mappers" href="http://cawm.lib.uiowa.edu/">Consortium of Ancient World Mappers</a>, 2022 (cc-by-4.0).',
+    oldawmcUrl = 'https://api.tiles.mapbox.com/v4/isawnyu.map-knmctlkh/{z}/{x}/{y}.png?access_token=' + token,
+    cawmUrl = 'http://cawm.lib.uiowa.edu/tiles/{z}/{x}/{y}.png';
 
 var osmAttr = 'OpenStreetMap',
     osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
@@ -35,9 +37,10 @@ var grayscale = L.tileLayer(mbUrl, {
         maxZoom: 11,
         attribution: dareAttr
     }),
-    awmc = L.tileLayer(awmcUrl, {
-        maxZoom: 12,
-        attribution: awmcAttr
+    cawm = L.tileLayer(cawmUrl, {
+        maxZoom: 11,
+        accessToken: token,
+        attribution: cawmAttr
     });
 
 var baseLayers = {
@@ -48,5 +51,6 @@ var baseLayers = {
     "Toner": toner,
     "Watercolor": watercolor,
     "Imperium": imperium,
-    "Ancient terrain": awmc
+    "Ancient terrain": cawm
 };
+
