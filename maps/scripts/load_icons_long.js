1bc9647e2bc52247a988c8278d530c4152cea55e
John Muccigrosso <john@muccigrosso.org>
2023-09-05 14:19:14 +0200

    Creating long-arrows

---
+++ maps/scripts/load_icons_long.js
@@ -0,0 +1,212 @@
+﻿// Define several icons of different colors
+// Originally from:
+// icons/marker-icon-2x-black.png
+// icons/marker-shadow.png
+
+var blackIcon = new L.Icon(
+{
+    iconUrl: '/icons/marker-icon-2x-black.png',
+    shadowUrl: '/icons/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+
+var whiteIcon = new L.Icon(
+{
+    iconUrl: '/icons/marker-icon-2x-white.png',
+    shadowUrl: '/icons/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+
+var greenIcon = new L.Icon(
+{
+    iconUrl: '/icons/marker-icon-2x-green.png',
+    shadowUrl: '/icons/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+
+var yellowIcon = new L.Icon(
+{
+    iconUrl: '/icons/marker-icon-2x-yellow.png',
+    shadowUrl: '/icons/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+
+var orangeIcon = new L.Icon(
+{
+    iconUrl: '/icons/marker-icon-2x-orange.png',
+    shadowUrl: '/icons/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+
+var greyIcon = new L.Icon(
+{
+    iconUrl: '/icons/marker-icon-2x-grey.png',
+    shadowUrl: '/icons/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+
+var blueIcon = new L.Icon(
+{
+    iconUrl: '/icons/marker-icon-2x-blue.png',
+    shadowUrl: '/icons/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+
+var redIcon = new L.Icon(
+{
+    iconUrl: '/icons/marker-icon-2x-red.png',
+    shadowUrl: '/icons/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+
+var BandWIcon = new L.Icon(
+{
+    iconUrl: '/icons/marker-icon-2x-b&w.png',
+    shadowUrl: '/icons/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+
+// var QMIcon = new L.Icon(
+// {
+//     iconUrl: '/icons/marker-icon-2x-QM.png',
+//     shadowUrl: '/icons/marker-shadow.png',
+//     iconSize: [25, 41],
+//     iconAnchor: [12, 41],
+//     popupAnchor: [1, -34],
+//     shadowSize: [41, 41]
+// });
+//
+
+// For rotations
+
+var arrowIcon = L.icon(
+{
+    iconUrl: '/icons/arrow-markerup_red_long.png',
+    iconAnchor: [18, 4096],
+    iconSize: [36, 4096]
+});
+
+var circleIcon = L.icon(
+{
+    iconUrl: '/icons/circle-marker_red.png',
+    iconSize: [12, 12]
+});
+
+// Circles for uncertain places
+
+var blackCircleIcon = new L.Icon(
+{
+    iconUrl: '/icons/marker-icon-2x-black-circle.png',
+    shadowUrl: '/icons/marker-shadow-circle.png',
+    iconSize: [25, 25],
+    iconAnchor: [12, 13],
+    shadowSize: [34, 35],
+    shadowAnchor: [12, 22]
+});
+
+var whiteCircleIcon = new L.Icon(
+{
+    iconUrl: '/icons/marker-icon-2x-white-circle.png',
+    shadowUrl: '/icons/marker-shadow-circle.png',
+    iconSize: [25, 25],
+    iconAnchor: [12, 13],
+    shadowSize: [34, 35],
+    shadowAnchor: [12, 22]
+});
+
+var greenCircleIcon = new L.Icon(
+{
+    iconUrl: '/icons/marker-icon-2x-green-circle.png',
+    shadowUrl: '/icons/marker-shadow-circle.png',
+    iconSize: [25, 25],
+    iconAnchor: [12, 13],
+    shadowSize: [34, 35],
+    shadowAnchor: [12, 22]
+});
+
+var yellowCircleIcon = new L.Icon(
+{
+    iconUrl: '/icons/marker-icon-2x-yellow-circle.png',
+    shadowUrl: '/icons/marker-shadow-circle.png',
+    iconSize: [25, 25],
+    iconAnchor: [12, 13],
+    shadowSize: [34, 35],
+    shadowAnchor: [12, 22]
+});
+
+var orangeCircleIcon = new L.Icon(
+{
+    iconUrl: '/icons/marker-icon-2x-orange-circle.png',
+    shadowUrl: '/icons/marker-shadow-circle.png',
+    iconSize: [25, 25],
+    iconAnchor: [12, 13],
+    shadowSize: [34, 35],
+    shadowAnchor: [12, 22]
+});
+
+var greyCircleIcon = new L.Icon(
+{
+    iconUrl: '/icons/marker-icon-2x-grey-circle.png',
+    shadowUrl: '/icons/marker-shadow-circle.png',
+    iconSize: [25, 25],
+    iconAnchor: [12, 13],
+    shadowSize: [34, 35],
+    shadowAnchor: [12, 22]
+});
+
+var blueCircleIcon = new L.Icon(
+{
+    iconUrl: '/icons/marker-icon-2x-blue-circle.png',
+    shadowUrl: '/icons/marker-shadow-circle.png',
+    iconSize: [25, 25],
+    iconAnchor: [12, 13],
+    shadowSize: [34, 35],
+    shadowAnchor: [12, 22]
+});
+
+var redCircleIcon = new L.Icon(
+{
+    iconUrl: '/icons/marker-icon-2x-red-circle.png',
+    shadowUrl: '/icons/marker-shadow-circle.png',
+    iconSize: [25, 25],
+    iconAnchor: [12, 13],
+    shadowSize: [34, 35],
+    shadowAnchor: [12, 22]
+});
+
+var BandWCircleIcon = new L.Icon(
+{
+    iconUrl: '/icons/marker-icon-2x-b&w-circle.png',
+    shadowUrl: '/icons/marker-shadow-circle.png',
+    iconSize: [25, 25],
+    iconAnchor: [12, 13],
+    shadowSize: [34, 35],
+    shadowAnchor: [12, 22]
+});
